61c0cc0a0eb44b149744fb4d7c48396b36ca1288
Daniel Tinsley <detinsley1s@gmail.com>
2017-09-04 04:47:00 -0500

    Generate card values on document load

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,6 +1,10 @@
 var MatchGame = {};
 
 // Sets up a new game after HTML document has loaded.
+$(function() {
+	MatchGame.renderCards(MatchGame.generateCardValues(), $("#body"));
+});
+
 // Renders a 4x4 board of cards.
 
 // Generates and returns an array of matching card values.
